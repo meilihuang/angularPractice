189aad64f983ac692a47689997dbda7d235f8303
meilihuang <c14180011@john.petra.ac.id>
2021-02-23 20:16:10 +0700

    Practice finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,88 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+
+//Initialization
+  board = [
+    ["*", "*", "*", "*", "*"],
+    ["*", "*", "*", "*", "*"],
+    ["*", "*", "*", "*", "*"],
+    ["*", "*", "*", "*", "*"],
+    ["*", "*", "*", "*", "*"]
+  ];
+
+  player1: boolean = true;
+  row = 0;
+  column = 0;
+
+  Process() {
+    //Player 1 
+    if (this.player1) {
+      //check whether the coordinate is already filled in or not
+      if (this.board[this.row][this.column] == "*") {
+        this.board[this.row][this.column] = "0";
+        this.player1 = false;
+      } else {
+        alert("It's already taken!");
+      }
+    } 
+    //Player 2
+    else {
+       //check whether the coordinate is already filled in or not
+      if (this.board[this.row][this.column] == "*") {
+        this.board[this.row][this.column] = "1";
+        this.player1 = true;
+      } else {
+        alert("It's already taken!");
+      }
+    }
+    
+    for (let i = 0; i < 5; i++) {
+      //check vertical Player 1
+      if (
+        this.board[i][0] == "0" &&
+        this.board[i][1] == "0" &&
+        this.board[i][2] == "0" &&
+        this.board[i][3] == "0" &&
+        this.board[i][4] == "0"
+      ) {
+        alert("PLAYER 1 WINS!");
+      }
+      //check vertical Player 2
+      if (
+        this.board[i][0] == "1" &&
+        this.board[i][1] == "1" &&
+        this.board[i][2] == "1" &&
+        this.board[i][3] == "1" &&
+        this.board[i][4] == "1"
+      ) {
+        alert("PLAYER 2 WINS!");
+      }
+      //check horizontal Player 1
+      if (
+        this.board[0][i] == "0" &&
+        this.board[1][i] == "0" &&
+        this.board[2][i] == "0" &&
+        this.board[3][i] == "0" &&
+        this.board[4][i] == "0"
+      ) {
+        alert("PLAYER 1 WINS!");
+      }
+      //check horizontal Player 2
+      if (
+        this.board[0][i] == "1" &&
+        this.board[1][i] == "1" &&
+        this.board[2][i] == "1" &&
+        this.board[3][i] == "1" &&
+        this.board[4][i] == "1"
+      ) {
+        alert("PLAYER 2 WINS!");
+      }
+    }
+  }
 }
